603328549ca162f63ff5c741e5397db7022d083d
XESTUS <xestus.office@gmail.com>
2026-08-06 11:31:38 +0530

    Completed Services section and added Portfolio section

--- script.js
+++ script.js
@@ -223,3 +223,23 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+lucide.createIcons();
+
+const serviceCards = document.querySelectorAll(".service-card");
+
+serviceCards.forEach(card => {
+
+    card.addEventListener("mousemove",(e)=>{
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty("--x", x + "px");
+        card.style.setProperty("--y", y + "px");
+
+    });
+
+});
